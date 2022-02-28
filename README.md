908d5ea476f884aa28e894f747ffe06f2e28f8ef
selsayed25 <2025selsayed@tjhsst.edu>
2022-02-27 22:08:59 -0500

    main

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # elsayed-tjhsst-blog
-This is Sami Elsayed's personal TJHSST blog.
+This is Sami Elsayed's personal TJHSST blog, built in Django, CSS, and HTML.
+
+Note: This website is still under work in progress, so expect to find errors, bugs, and/or gliches.
---
+++ hello/views.py
@@ -0,0 +1,27 @@
+from nis import match
+import re
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.timezone import datetime
+
+# Create your views here.
+
+def home(request):
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
+
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
